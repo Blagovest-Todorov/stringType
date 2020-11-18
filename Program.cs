ba97d73d5150991494eddeaabd146c7ab89f5098
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-18 20:11:18 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace stringIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "String is a primitive data type , but acts like referent dataType";
+            Console.WriteLine("What is string ?");
+            Console.WriteLine(text);
+        }
+    }
+}
